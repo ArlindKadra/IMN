fbda3bd8c04906b736ee419225430ddfd48e742f
Arlind Kadra <arlindkadra@gmail.com>
2024-02-01 11:34:43 +0100

    Update hypernetwork.py

--- models/hypernetwork.py
+++ models/hypernetwork.py
@@ -1,8 +1,6 @@
 import torch
 import torch.nn as nn
 
-from models.exu import LogLinear
-
 
 class HyperNet(nn.Module):
     def __init__(
@@ -12,7 +10,6 @@ class HyperNet(nn.Module):
         nr_blocks: int = 0,
         hidden_size: int = 64,
         dropout_rate: float = 0.25,
-        unit_type: str = "basic",
         **kwargs,
     ):
         super(HyperNet, self).__init__()
@@ -30,10 +27,7 @@ class HyperNet(nn.Module):
         for _ in range(nr_blocks):
             self.blocks.append(self.make_residual_block(hidden_size, hidden_size, dropout_rate=self.dropout_rate))
 
-        if unit_type != "basic":
-            self.output_layer = LogLinear(hidden_size, (nr_features + 1) * nr_classes)
-        else:
-            self.output_layer = nn.Linear(hidden_size, (nr_features + 1) * nr_classes)
+        self.output_layer = nn.Linear(hidden_size, (nr_features + 1) * nr_classes)
 
         for m in self.modules():
             if isinstance(m, (nn.BatchNorm1d, nn.GroupNorm)):
